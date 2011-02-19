0ca6aaa8569f6aa82b2c1577ad13c758c934e97e
Erik Jansson <deflare@gmail.com>
2011-02-19 16:05:05 +0100

    Tidied up code

--- nowplaying.class.php
+++ nowplaying.class.php
@@ -9,44 +9,43 @@
 */
 
 class NowPlaying{
-    
+
     private $url;
     private $noTrackPlayingMessage;
-    
-    function __construct($user, $api_key){
-        
-        //	construct URL
-		$this->url  = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1';
-		$this->url .= '&user=' . $user . '&api_key=' . $api_key;
-		
-		// default message
-		$this->noTrackPlayingMessage = 'Nothing is playing right now!';
-    }
-	
-	public function getNowPlaying(){
 
-		//	create an XML object
-		$xml = simplexml_load_file($this->url);
-		
-		//	get the latest track
-		$track = $xml->recenttracks->track;
-		
-		// check if the track is actually playing 
-		$nowplaying = $track->attributes()->nowplaying;
-	
-		if($nowplaying){
-			$artist = $track->artist;
-			$songname = $track->name;
-			return $artist . ' - ' . $songname;
-		}
-		else{
-			return $this->noTrackPlayingMessage;
-		}
-		
-	}
-	
-	public function setNoTrackPlayingMessage($messageIn){
-	    $this->noTrackPlayingMessage = $messageIn;
-	}
-	
+    function __construct($user, $api_key){
+
+        // construct URL
+        $this->url  = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1';
+        $this->url .= '&user=' . $user . '&api_key=' . $api_key;
+
+        // default message
+        $this->noTrackPlayingMessage = 'Nothing is playing right now!';
+    }
+
+    public function getNowPlaying(){
+
+        //	create an XML object
+        $xml = simplexml_load_file($this->url);
+
+        //	get the latest track
+        $track = $xml->recenttracks->track;
+
+        // check if the track is actually playing 
+        $nowplaying = $track->attributes()->nowplaying;
+
+        if($nowplaying){
+            $artist = $track->artist;
+            $songname = $track->name;
+            return $artist . ' - ' . $songname;
+        }
+        else{
+            return $this->noTrackPlayingMessage;
+        }
+    }
+
+    public function setNoTrackPlayingMessage($messageIn){
+        $this->noTrackPlayingMessage = $messageIn;
+    }
+
 } // end class
