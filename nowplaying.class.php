1cd3622b20dbb65d94bc8f4125b5ba32707e37ef
Erik Jansson <deflare@gmail.com>
2011-02-19 15:46:37 +0100

    First commit. Few features

---
+++ nowplaying.class.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+* Class that returns the track a user is currently playing via Last.fm
+*
+* @author       Erik Jansson <erik@eriknow.com>
+* @copyright    Erik Jansson 2011
+*
+*/
+
+class NowPlaying{
+    
+    private $url;
+    private $noTrackPlayingMessage;
+    
+    function __construct($user, $api_key){
+        
+        //	construct URL
+		$this->url  = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1';
+		$this->url .= '&user=' . $user . '&api_key=' . $api_key;
+		
+		// default message
+		$this->noTrackPlayingMessage = 'Nothing is playing right now!';
+    }
+	
+	public function getNowPlaying(){
+
+		//	create an XML object
+		$xml = simplexml_load_file($this->url);
+		
+		//	get the latest track
+		$track = $xml->recenttracks->track;
+		
+		// check if the track is actually playing 
+		$nowplaying = $track->attributes()->nowplaying;
+	
+		if($nowplaying){
+			$artist = $track->artist;
+			$songname = $track->name;
+			return $artist . ' - ' . $songname;
+		}
+		else{
+			return $this->noTrackPlayingMessage;
+		}
+		
+	}
+	
+	public function setNoTrackPlayingMessage($messageIn){
+	    $this->noTrackPlayingMessage = $messageIn;
+	}
+	
+} // end class
+	
+?>
